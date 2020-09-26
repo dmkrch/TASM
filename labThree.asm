a79220fe5552a4d5a872f9bdf57a4e95a094aae8
Dmitriy <dmkrch1@gmail.com>
2020-09-26 17:08:55 +0300

    added content form 2 lab

--- labThree.asm
+++ labThree.asm
@@ -1,10 +1,17 @@
 model small
 .stack 100h
 .data
-firstN db "write first number: ", 10, 13, '$'
-secondN db "write second number: ", 10, 13, '$'
+a dw ?
+b dw ?
+c dw ?
+d dw ?
+firstN db "write a: ", 10, 13, '$'
+secondN db "write b: ", 10, 13, '$'
+thirdN db "write c: ", 10, 13, '$'
+fourthN db "write d number: ", 10, 13, '$'
 errMsg db "overflow of number, try again", 10, 13, '$'
-result db "sum = $"
+result db "result = $"
+zeroMsg db "can't divide by zero", 10, 10, '$'
 ten dw 10	
 sign db 0
 .code
@@ -67,9 +74,7 @@ symbLes:mov cl, al		; in cl - code of symbol
 lp4:	call delete
 	loop lp4
 	
-	mov ah, 09h
-	lea dx, errMsg
-	int 21h
+	call carry
 	
 	xor bx, bx
 	jmp lp1
@@ -104,9 +109,10 @@ delete endp
 
 ; procedure that outputs number in bx
 output proc
-	push cx
 	push ax
 	push bx
+	push cx
+	push dx
 
 	xor cx, cx
 	mov ax, bx
@@ -138,40 +144,159 @@ lp3:	pop dx
 
 	loop lp3
 
-end_lp2:pop bx
-	pop ax
+end_lp2:pop dx
 	pop cx
+	pop bx	
+	pop ax
 	ret
 output endp	
 
+;procedure that printf error message if overflow
+carry proc
+	push ax
+	push dx
 	
+	lea dx, errMsg
+	mov ah, 09h
+	int 21h
+	
+	pop dx
+	pop ax
 
+	ret
+carry endp
+	
 start:	mov ax, @data
 	mov ds, ax
-
-	;input of first number
-	mov ah, 09h
+;input of numbers
 	lea dx, firstN
-	int 21h	
-	call input		; now in bx - input number		
-	
-	push bx			; in stack - first number
-;input of second number	
 	mov ah, 09h
+	int 21h
+	call input
+	mov [a], bx
+
 	lea dx, secondN
+	int 21h	
+	call input		
+	mov [b], bx
+	
+	lea dx, thirdN
 	int 21h
-	call input 		; in bx - second number
-;calculating sum of numbers
-	pop ax			; in ax - first number
-	add bx, ax		; in bx: first number + second number	
+	call input	
+	mov [c], bx
 
-;printing result of sum
+	lea dx, fourthN
+	int 21h
+	call input
+	mov [d], bx
+
+
+;now calculating
+	mov ax, [a]	; in ax - a
+	mul [a]		; in ax - a^2
+
+	jnc next
+	call carry	; printf if overflow
+	jmp ending
+	
+next:	push ax		; a^2	in stack	
+
+	mov ax, [b]	; in ax - b
+	mul [b]
+	
+	jnc next1
+	call carry
+	jmp ending
+
+next1:	mul [b]		; in ax - b^3
+
+	jnc next2
+	call carry
+	jmp ending
+		
+next2:	pop bx		; in bx - a^2 
+	cmp bx, ax	; comparing a^2 and b^3
+	jge bxLess	; if a^2 >= b^3 go bxLess
+; if not - continkkue
+	mov ax, [c]	; in ax - c
+	mul [b]		; in ax - c * b
+
+	jnc next3
+	call carry
+	jmp ending
+
+next3:	push ax		; in stack - c * b
+	
+	mov ax, d	; in ax - d
+	div b		; in ax - d / b
+	
+	pop bx		; in bx - c * b
+	
+	cmp ax, bx 	; comparing c * b and d / b
+	je axEqbx	; if c*b == d/b go axEqbx
+	
+	mov ax, [a]	;
+	or ax, [b]	; in ax - a OR b
+
+; ax is ready, now we need to exit
+	jmp finalRes
+axEqbx:	mov ax, [a]	; in ax - a
+	mul [a]		; in ax - a^2
+	mul [a]		; in ax - a^3
+	push ax		; in stack - a^3
+	mov ax, [b]
+	mul [b]		; in ax - b ^2
+	push ax		; stack: 1) b^2 2) a^3
+	mov ax, [c]
+	mul [c]		; in ax - c^2
+	mov bx, ax	; in bx - c ^2
+	pop ax		; in ax - b^2
+	sub ax, bx	; in ax - b^2 - c^2
+	mov bx, ax	; in bx - b^2 - c^2
+
+	cmp bx, 0
+	jnz notZero
+	lea dx, zeroMsg
 	mov ah, 09h
+	int 21h
+	jmp ending
+
+	pop ax		; in ax - a^3
+notZero:div bx		; in ax - a^3 / (b^2 - c^2)
+	
+	push ax		; in stack - a^3 / (...)
+	mov ax, [b]
+	mul [b]		; in ax - b^2
+	add ax, 24	; in ax - b^2 + 24
+	push ax		; in stack - 1) a^3 / (...) 2) b^2 + 24
+
+	mov ax, [d]
+	mul [d]		; in ax - d^2	
+	mov bx, ax	; in bx - d^2
+	
+	pop ax		; in ax - b^2 + 24
+
+	div bx		; in ax - (b^2 + 24) / d^2
+
+	pop bx		; in bx - a^3 / (...)	
+	add ax, bx	; in ax - a^3 / (b^2 - c^2) + (b^2+24)/d^2
+	
+	
+; ax is ready, now we need to exit
+	jmp finalRes	; ax is ready, now we need to exit		
+bxLess:	mov ax, [a]	; in ax - a
+	mul [c]		; in ax - c * a
+	sub ax, [b]	; in ax - c * a - b
+; ax is ready	
+finalRes:
+	mov bx, ax
+
 	lea dx, result
-	int 21h
-	call output		; outputting bx number	
-
-	mov ah, 4ch
+	mov ah, 09h
 	int 21h
 
+	call output
+		
+ending:	mov ah, 4ch
+	int 21h
 end start
