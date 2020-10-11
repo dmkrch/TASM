fae85f99efefac52295b9f2127442c6946a60155
Dmitriy <dmkrch1@gmail.com>
2020-10-11 13:02:05 +0300

    vowel or consonant check added

--- labFour.asm
+++ labFour.asm
@@ -1,23 +1,52 @@
 model small
 .stack 100h
 .data
-vowel_message db "number is vowel", 10, 13, '$'
-consonant_message db "number is consonant", 10, 13, '$'
+vowel_message db "letter is vowel", 10, 13, '$'
+consonant_message db "letter is consonant", 10, 13, '$'
+not_symbol_message db "not a letter", 10, 13, '$'
 
 vowels_array db 65, 69, 73, 79, 85, 89, 97, 101, 105, 111, 117, 121
 vowels_arr_len db 12
 
+symbol_status db (?)
+
 .code
 Vowel_Or_Consonant proc
 	cld
-	mov cx, 12
+	mov cl, [vowels_arr_len]
 	lea di, vowels_array
-	rep scasb		
+
+	repne scasb
+	je is_vowel
 	
-	mov dl, [di]
-	mov ah, 02h
+	;( al >= 64 && al <= 90) || (al >= 97 && al  <= 122)
+	cmp al, 64
+	jl not_letter
+	cmp al, 90
+	jg next_comp
+	jmp is_consonant
+
+next_comp:	
+	cmp al, 97
+	jl not_letter
+	cmp al, 122
+	jg not_letter
+is_consonant:
+	lea dx, consonant_message
+	mov ah, 09h
 	int 21h
-	
+	jmp end_procedure
+not_letter:
+	lea dx, not_symbol_message
+	mov ah, 09h
+	int 21h
+	jmp end_procedure
+is_vowel:
+	dec di
+	lea dx, vowel_message
+	mov ah, 09h
+	int 21h
+end_procedure:	
 	ret
 Vowel_Or_Consonant endp		
 
@@ -26,10 +55,12 @@ Vowel_Or_Consonant endp
 start:
 	mov ax, @data
 	mov ds, ax
+	mov es, ax
 
 	xor ax, ax
-	mov al, [vowels_array + 5] ; moving to 5th vowel symbol to al
-
+	mov ah, 01h
+	int 21h
+	
 	call Vowel_Or_Consonant
 	
 	mov ah, 4ch
